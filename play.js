bdf0986425a34533e6b838b10fc15636efabaf88
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-11-04 00:01:10 -0400

    move commands changes

--- play.js
+++ play.js
@@ -3,7 +3,49 @@ const conn = connect();
 const handleEvents = () => {
   // code that does something
   //console.log("Successfully connected to game server");
-  conn.write("Name: GKP");
+  conn.write("Name: GK");
+  setInterval(() => {
+    //conn.write("Move: up");
+  },50);
+
+  setTimeout(() => {
+    setInterval(() => {
+      //conn.write("Move: left");
+    },51);
+  },200);
+
+  setTimeout(() => {
+    setInterval(() => {
+      // conn.write("Move: right");
+    },52);
+  },800);
+
+  //1.
+  //conn.write("Name: GK");
+  //conn.write("Move: up");
+  //conn.write("Move: up");
+  //conn.write("Move: up");
+
+  //2.
+
+  /*conn.write("Name: GK");
+  setTimeout(() => {
+    conn.write("Move: up");
+  },50);
+  setTimeout(() => {
+    conn.write("Move: up");
+  },50);
+  setTimeout(() => {
+    conn.write("Move: up");
+  },50); */
+
+  //3.
+
+  //conn.write("Name: GK");
+  /* setInterval(() => {
+    //conn.write("Move: up");
+  },50); */
 };
 
 conn.on("connect", handleEvents);
+
